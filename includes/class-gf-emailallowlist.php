30098b92484e61d40c50d318c568c41c698c8d82
Rob Migchels <rob.migchels@nbg.nl>
2024-07-12 09:58:03 +0200

    String updates

--- includes/class-gf-emailallowlist.php
+++ includes/class-gf-emailallowlist.php
@@ -12,15 +12,15 @@ GFForms::include_addon_framework();
 
 class GFEmailAllowlist extends GFAddOn {
 
-	protected $_version = '1.0.0';
+	protected $_version                  = '1.0.0';
 	protected $_min_gravityforms_version = '2.8';
-	protected $_slug = 'gf_email_allowlist';
-	protected $_path = 'gravity-forms-email-allowlist/gf-emailallowlist.php';
-	protected $_full_path = __FILE__;
-	protected $_title = 'Gravity Forms Email Allowlist';
-	protected $_short_title = 'Email Allowlist';
+	protected $_slug                     = 'gf_email_allowlist';
+	protected $_path                     = 'gravity-forms-email-allowlist/gf-emailallowlist.php';
+	protected $_full_path                = __FILE__;
+	protected $_title                    = 'Gravity Forms Email Allowlist';
+	protected $_short_title              = 'Email Allowlist';
 
-    private static $_instance = null;
+	private static $_instance = null;
 
 	/**
 	 * Get an instance of this class.
@@ -35,7 +35,7 @@ class GFEmailAllowlist extends GFAddOn {
 		return self::$_instance;
 	}
 
-    /**
+	/**
 	 * Add tasks or filters here that you want to perform only in admin.
 	 */
 	public function init_admin() {
@@ -63,10 +63,9 @@ class GFEmailAllowlist extends GFAddOn {
 	public function get_menu_icon() {
 
 		return 'gform-icon--mail';
-
 	}
 
-    /**
+	/**
 	 * Add the additional Email Allowlist
 	 *
 	 * @return array Additional plugin setting fields in the Gravity Forms Settings API.
@@ -75,29 +74,26 @@ class GFEmailAllowlist extends GFAddOn {
 		return array(
 			array(
 				'title'       => esc_html__( 'Email Allowlist Global Settings', 'gf-email-allowlist' ),
-				'description' => esc_html__( 'Use Email Allowlist to secure your forms. If a allowlisted email is used in any email field, the form will error on submission. You can also globally define a list of allowlisted emails and/or domains and a custom validation message if a allowlisted email is submitted. These settings can be overridden on individual email fields in the advanced settings.', 'gf-email-allowlist' ),
+				'description' => esc_html__( 'Use Email Allowlist to secure your forms. If a domain extension is used that isn\'t on your allowlist, the form will throw an error on submission. You can enable and disable this allowlist for each email field on your forms.', 'gf-email-allowlist' ),
 				'fields'      => array(
 					array(
 						'label'   => __( 'Allowlisted Emails', 'gf-email-allowlist' ),
 						'type'    => 'textarea',
 						'name'    => 'emailallowlist',
-						'tooltip' => __( 'Please enter a comma separated list of allowlisted domains (ex. hotmail.com), email addresses (ex. user@aol.com), and/or include the wildcard notation to block top-level domains (ex. *.com). This setting can be overridden on individual email fields in the advanced settings.', 'gf-email-allowlist' ),
-						'class'   => 'medium',
+						'tooltip' => __( 'One domain extension (ex. *.com) per line.', 'gf-email-allowlist' ),
 					),
 					array(
 						'label'   => esc_html__( 'Validation Message', 'gf-email-allowlist' ),
 						'type'    => 'text',
 						'name'    => 'emailallowlist_error_msg',
-                        'default' => __( 'Sorry, the email address entered is not eligible for this form', 'gf-email-allowlist' ),
-						'tooltip' => esc_html__( 'Please enter a default error message if a allowlisted email is submitted.', 'gf-email-allowlist' ),
-						'class'   => 'medium',
+						'tooltip' => esc_html__( 'Please enter an error message if a allowlisted email isn\'t submitted. ', 'gf-email-allowlist' ),
 					),
 				),
 			),
 		);
 	}
 
-    /**
+	/**
 	 * Add email allowlist setting to the email fields advanced settings.
 	 *
 	 * @param integer $position Specifies the position that the settings will be displayed.
@@ -109,31 +105,31 @@ class GFEmailAllowlist extends GFAddOn {
 		if ( 1375 === $position ) {
 			?>
 
-        <li class="rmgf field_setting">
-            <input type="checkbox" id="field_rmgf_allowlist" onclick="SetFieldProperty('rmgf_email_allowlist', this.checked);" onkeypress="SetFieldProperty('rmgf_email_allowlist', this.checked);" />
-            <label for="field_rmgf_allowlist" class="inline">
-                <?php esc_html_e( 'Enable Email Allowlist', 'gf-email-allowlist' ); ?>
+		<li class="rmgf field_setting">
+			<input type="checkbox" id="field_rmgf_allowlist" onclick="SetFieldProperty('rmgf_email_allowlist', this.checked);" onkeypress="SetFieldProperty('rmgf_email_allowlist', this.checked);" />
+			<label for="field_rmgf_allowlist" class="inline">
+				<?php esc_html_e( 'Enable Email Allowlist', 'gf-email-allowlist' ); ?>
 				<?php gform_tooltip( 'form_field_rmgf_enable_allowlist' ); ?>
-            </label>
-        </li>
+			</label>
+		</li>
 			<?php
 		}
 	}
 
-    /**
+	/**
 	 * Add the additional tooltips to the new fields.
 	 *
 	 * @param array $tooltips tooltip associative array.
 	 * @return array modified tooltips
 	 */
 	public function rmgf_field_tooltips( $tooltips ) {
-        $admin_url = get_admin_url() . 'admin.php?page=gf_settings&subview=ibrains-gf';
-		$tooltips['form_field_rmgf_enable_allowlist']      = __( "<strong>Enable Email Allowlist</strong>Enable the global allowlist for this field as defined in the <a href=\"$admin_url\" target=\"_blank\">form settings</a>.", 'gf-email-allowlist' );
+		$admin_url                                    = get_admin_url() . 'admin.php?page=gf_settings&subview=ibrains-gf';
+		$tooltips['form_field_rmgf_enable_allowlist'] = __( "<strong>Enable Email Allowlist</strong>Enable the global allowlist for this field as defined in the <a href=\"$admin_url\" target=\"_blank\">form settings</a>.", 'gf-email-allowlist' );
 
-        return $tooltips;
+		return $tooltips;
 	}
 
-    /**
+	/**
 	 * Inject Javascript into the form editor page for the email allowlist fields.
 	 */
 	public function rmgf_gform_editor_js() {
@@ -144,15 +140,15 @@ class GFEmailAllowlist extends GFAddOn {
 			fieldSettings["email"] = fieldSettings["email"] + ", .rmgf.field_setting"; // this will show all fields that Paragraph Text field shows plus my custom setting
 
 			// Binding to the load field settings event to initialize the checkbox.
-            $(document).bind('gform_load_field_settings', function(event, field, form){
-                jQuery( '#field_rmgf_allowlist' ).prop( 'checked', Boolean( rgar( field, 'rmgf_email_allowlist' ) ) );
-            });
+			$(document).bind('gform_load_field_settings', function(event, field, form){
+				jQuery( '#field_rmgf_allowlist' ).prop( 'checked', Boolean( rgar( field, 'rmgf_email_allowlist' ) ) );
+			});
 		});
 	</script>
 		<?php
 	}
 
-    /**
+	/**
 	 * Add email allowlist to gforms validation function.
 	 *
 	 * @resources: https://docs.gravityforms.com/using-gform-validation-hook/
@@ -162,7 +158,7 @@ class GFEmailAllowlist extends GFAddOn {
 	 */
 	public function rmgf_validation( $validation_result ) {
 
-        // Collect global settings.
+		// Collect global settings.
 		$default_allowlist = get_option( 'gravityformsaddon_' . $this->_slug . '_settings' );
 		if ( is_array( $default_allowlist ) && ! empty( $default_allowlist['emailallowlist'] ) ) {
 			$default_allowlist = $default_allowlist['emailallowlist'];
@@ -245,8 +241,8 @@ class GFEmailAllowlist extends GFAddOn {
 			$field['failed_validation']    = true;
 
 			// Set a default validation message.
-			$default_message = __( 'Sorry, the email address entered is not eligible for this form.', 'gf-email-allowlist' );
-			$settings_option = get_option( 'gravityformsaddon_' . $this->_slug . '_settings' );
+			$default_message    = __( 'Sorry, the email address entered is not eligible for this form.', 'gf-email-allowlist' );
+			$settings_option    = get_option( 'gravityformsaddon_' . $this->_slug . '_settings' );
 			$validation_message = $default_message;
 
 			// Check if the settings option exists and contains the specific error message.
@@ -270,10 +266,9 @@ class GFEmailAllowlist extends GFAddOn {
 
 		$validation_result['form'] = $form;
 		return $validation_result;
+	}
 
-    }
-
-    /**
+	/**
 	 * Convert a sting to lowercase and remove extra whitespace. Thanks to @ractoon, @rscoates.
 	 *
 	 * @param string $string A string to sanitize.
@@ -282,5 +277,4 @@ class GFEmailAllowlist extends GFAddOn {
 	protected function rmgf_clean( $string ) {
 		return strtolower( trim( $string ) );
 	}
-
 }
